072783092916b2a27fce0e9eb3f8723933d04fac
VivekDudi-Github <vivekdudi18@gmail.com>
2025-10-18 23:58:12 +0530

    Enhance LiveChat and WatchLive components: add message blocking functionality, improve message display with avatars and options menu, and implement collapsible chat section in WatchLive.

--- Frontend/src/component/live/LiveChats.jsx
+++ Frontend/src/component/live/LiveChats.jsx
@@ -1,15 +1,27 @@
 import { useEffect, useRef, useState } from "react";
-import {ArrowDownIcon} from 'lucide-react' ;
+import {ArrowDownIcon, EllipsisVerticalIcon} from 'lucide-react' ;
+import { useSocket } from "../specific/socket";
 
-export default function LiveChat() {
+export default function LiveChat({closeFunc}) {
+  const socket = useSocket();
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState("");
   const containerRef = useRef(null);
 
+  const [openOptions , setOpenOptions] = useState(new Set()) ;
+  const [BlockList , setBlockList] = useState(new Set('123')) ;
+  console.log(BlockList);
+  
+
+
   const sendMessage = () => {
     if (!input.trim()) return;
-    setMessages([...messages, { user: "You", text: input }]);
+    setMessages([...messages, { user: "1", text: input }]);
     setInput("");
+    socket.emit("send-Live-message", {
+      message: input,
+      roomId: "1",
+    });
   };
 
   useEffect(() => {
@@ -38,16 +50,43 @@ export default function LiveChat() {
       behavior: 'smooth',
     });
   };
+  const toggleSetOptions = (i) => {
+    if (openOptions.has(i)) {
+      let set = new Set(openOptions);
+      set.delete(i);
+      setOpenOptions(set);
+    } else {
+      let set = new Set(openOptions);
+      set.add(i); 
+      setOpenOptions(set);
+    }
+  }
 
   return (
     <div className="relative w-full h-full overflow-hidden">
     {/* Messages area */}
-    <div ref={containerRef} className="absolute top-0 left-0 right-0 bottom-[50px] overflow-y-auto p-3 space-y-2 ">
-      {messages.map((msg, i) => (
-        <div key={i} className="bg-gray-100 w-full text-wrap break-words p-2 rounded-lg fade-in duration-200">
-          <strong>{msg.user}: </strong>{msg.text}
+    <div ref={containerRef} className="absolute top-0 left-0 right-0 bottom-[50px] overflow-y-auto p-3 space-y-2 z-10">
+      {messages.map((msg, i) => {
+        return(
+        <div key={i} className={`bg-gray-100 w-full flex items-center justify-between gap-1 text-wrap break-words p-2 rounded-lg fade-in duration-200 relative ${openOptions?.has(i) ? 'z-50' : 'z-0' } `}>
+          <div className=" w-fit">
+            <img className="rounded-full size-8 mr-1" src={msg.user === "You" ? "https://i.pravatar.cc/300?img=1" : "https://i.pravatar.cc/300?img=2"} alt="Avatar" />
+            
+          </div>
+          <div className="w-full text-left"><strong>You_fsfdxgf</strong> {' : '}{BlockList.has(msg.user) ? (<i>Blocked</i>) : msg.text}</div>
+          <div className="relative" onClick={() => toggleSetOptions(i)}>
+            <EllipsisVerticalIcon size className="text-gray-500 hover:text-gray-700 cursor-pointer size-5 z-0 " />
+            {openOptions?.has(i) && (
+              <div className="absolute top-8 right-0 bg-white rounded-lg shadow-lg hover:cursor-pointer space-y-1 border border-gray-200 z-50 overflow-hidden">
+                <div className=" hover:bg-slate-200 duration-200 px-2">{BlockList.has(msg.user) ? 'Unblock' : 'Block'}</div>
+                <div className=" hover:bg-slate-200 duration-200 px-2">Report</div>
+                <div className=" hover:bg-slate-200 duration-200 px-2">Delete</div>
+              </div>
+            )}
+          </div>
         </div>
-      ))}
+        )
+      })}
     </div>
 
     <div>
--- Frontend/src/component/live/WatchLive.jsx
+++ Frontend/src/component/live/WatchLive.jsx
@@ -4,14 +4,15 @@ import LiveChat from "./LiveChats";
 import { useGetLiveStreamQuery } from "../../redux/api/api";
 import { useSocket } from "../specific/socket";
 import { useMediasoupConsumers } from "../specific/broadcast/RecieveBroadcast";
-import { useParams } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import VideoPlayer from "../specific/VideoPlayer";
-import { StopCircle , UserPlus2Icon , UserRoundCheckIcon , BookmarkCheckIcon , BookmarkIcon , Share2Icon } from "lucide-react";
+import { StopCircle , UserPlus2Icon , UserRoundCheckIcon , BookmarkCheckIcon , BookmarkIcon , Share2Icon, SidebarOpenIcon } from "lucide-react";
 
 export default function WatchLive({localStreamRef , stopBroadcast , isProducer}) {
   const {id} = useParams() ;
   const socket = useSocket() ;
 
+  const [collapse , setCollapse] = useState(false) ;
   const [activeStream , setActiveStream] = useState({
     videoStream : localStreamRef?.current?.videoStream || null ,
     audioStream : localStreamRef?.current?.audioStream || null ,
@@ -56,7 +57,7 @@ export default function WatchLive({localStreamRef , stopBroadcast , isProducer})
       <div className="flex flex-col lg:flex-row w-full justify-center  dark:bg-black h-full">
         {/* This is the mediasoup consumer video */}
         {activeStream && (activeStream.audioStream || activeStream.videoStream) && (
-          <div className="w-full h-full flex-grow  ">
+          <div className="w-full h-full flex-grow  overflow-y-scroll ">
             <div className="flex-1">
               <VideoPlayer stream={activeStream?.videoStream} audioStream={activeStream?.audioStream}  />
             </div>
@@ -64,7 +65,7 @@ export default function WatchLive({localStreamRef , stopBroadcast , isProducer})
             {/* Stream Info Section */}
             <div className="p-4 flex flex-col gap-4  dark:text-white rounded-md shadow-md  flex-1 overflow-y-auto ">
               {/* Profile & Actions */}
-              <div className="flex items-center justify-between">
+              <div className="flex items-center gap-3 justify-between overflow-auto">
                 {/* Left Side: Profile */}
                 <div className="flex items-center gap-3">
                   <img
@@ -72,14 +73,11 @@ export default function WatchLive({localStreamRef , stopBroadcast , isProducer})
                     alt="Profile"
                     className="w-10 h-10 rounded-full border border-gray-700"
                   />
-                  <div>
-                    <a
-                      href={activeStream?.host?.profileUrl || '#'}
-                      className="font-semibold hover:underline"
-                    >
+                  <div className="pt-1">
+                    <NavLink to={`/profile/${activeStream?.host?.username}`} className="font-semibold hover:underline text-sm  flex ">
                       {activeStream?.host?.name || 'Unknown Streamer'}
-                    </a>
-                    <p className="text-sm text-gray-400 ">
+                    </NavLink>
+                    <p className="text-xs text-gray-400 ">
                       {activeStream?.host?.followersCount ?? 0} followers
                     </p>
                   </div>
@@ -91,6 +89,7 @@ export default function WatchLive({localStreamRef , stopBroadcast , isProducer})
                   {!isProducer && <ControlButton ><UserPlus2Icon /></ControlButton> }
                   <ControlButton><BookmarkCheckIcon /></ControlButton>
                   <ControlButton ><Share2Icon /></ControlButton>
+                  <ControlButton onClick={() => setCollapse(prev => !prev)}><SidebarOpenIcon /></ControlButton>
                 </div>
               </div>
 
@@ -104,24 +103,14 @@ export default function WatchLive({localStreamRef , stopBroadcast , isProducer})
                 </div>
                 <p className="mt-2 text-gray-300 text-sm whitespace-pre-wrap">
                   {activeStream?.description || 'No description available.Desciption lorem'} 
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum delectus voluptatem ipsam iste maxime amet explicabo? Facere, animi consequatur excepturi possimus atque praesentium iusto cumque error aspernatur quia at incidunt.
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur at sed ratione earum, totam labore amet temporibus ipsum praesentium explicabo debitis ullam laudantium quasi, inventore impedit eaque et, odit laborum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, voluptas.
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed, delectus. Autem cupiditate molestias quasi tenetur magnam. Est quia quas maxime doloremque cum delectus ut consequuntur repudiandae sunt, atque perspiciatis omnis.
-                  Amet, aspernatur sequi illum ullam quidem laborum repudiandae est odit deleniti architecto omnis provident tenetur reprehenderit expedita voluptatum repellendus commodi dolorem. Minima voluptates, ullam laboriosam dolorem optio excepturi impedit necessitatibus.
-                  Ducimus vero harum maiores ipsum molestiae mollitia, enim odit libero blanditiis quis repudiandae eum maxime. Sequi delectus fugiat porro reprehenderit itaque, repudiandae, nihil repellendus minima placeat veniam corporis, rerum facere?
-                  Repudiandae numquam, laborum minus distinctio voluptatum quam adipisci deserunt optio velit sit, dolore deleniti sunt ut recusandae nemo atque? Iusto officiis tempora deserunt sunt quaerat, veritatis aperiam cumque necessitatibus earum?
-                  Tenetur facere necessitatibus delectus recusandae, dignissimos voluptas soluta maxime. Quod, autem quisquam eveniet repellendus quaerat et odio numquam iure facilis provident esse doloribus, ea magni reiciendis est suscipit, aliquid enim.
-                  Quas dolore nihil inventore veniam soluta. Assumenda cupiditate architecto quis rerum maxime possimus optio repellendus blanditiis repudiandae nisi est reprehenderit ab earum rem iure minima, consequatur voluptates? Mollitia, facere soluta!
-                  Facere obcaecati nisi sapiente, voluptatem assumenda nam repellat illo eligendi consequatur eos delectus nihil, cumque, officiis corporis. Labore, tenetur dolores modi delectus natus necessitatibus vitae sapiente asperiores voluptas. Rerum, nostrum!
-                  Soluta, suscipit commodi facere ipsam assumenda et eaque consectetur, molestias ea dolore iure sapiente incidunt quisquam. Velit id ducimus, ut voluptates dicta quia neque at praesentium dolorem obcaecati explicabo reprehenderit?
-                  Ullam ea quaerat assumenda illo reiciendis quas iure voluptatum enim commodi, aliquid quo, in ipsa accusamus totam consequatur modi facere dolore dolor odit temporibus magnam! Impedit mollitia cumque necessitatibus recusandae?
                   Reprehenderit placeat vero doloribus rem nesciunt. Fugiat veritatis vitae perferendis dolorum odit nisi natus architecto enim voluptate accusamus, numquam consequuntur rem. Pariatur recusandae fuga quidem animi exercitationem corporis ducimus suscipit.
                 </p>
               </div>
             </div>
           </div>
         )}
-        <div className=" h-full lg:w-1/2 w-full flex  "><LiveChat /></div>
+        <div className={`flex duration-200 ${collapse ? 'w-0 h-0' : 'lg:w-1/2 w-full' } `}><LiveChat closeFunc={() => setCollapse(true)} /></div>
+        
       </div>
     </div> 
     
@@ -131,10 +120,10 @@ function ControlButton({ children, label, danger , onClick }) {
   return (
     <button
       onClick={onClick}
-      className={`flex flex-col items-center justify-center gap-1 px-1 py-1 rounded-lg shadow-sm hover:shadow-md transition active:scale-90 ${danger ? 'bg-red-600 text-white' : ' bg-white text-black hover:bg-gray-200'} shadowLight `} 
+      className={`  items-center text-xs justify-center gap-1 px-1 py-1 rounded-lg  transition active:scale-90 ${danger ? 'bg-red-600 text-white' : ' bg-white text-black hover:bg-gray-200'} shadowLight `} 
     >
       {children}
-      <span className={`text-[11px] ${danger ? 'text-white' : 'text-slate-400'}`}>{label}</span>
+      <span className={`sm:text-[11px] ${danger ? 'text-white' : 'text-slate-400'}`}>{label}</span>
     </button>
   );
 }
