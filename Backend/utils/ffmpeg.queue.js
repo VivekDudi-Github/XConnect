e35efa39f91634e2994efb7902d4f9504c68d51b
VivekDudi-Github <vivekdudi18@gmail.com>
2026-06-01 12:27:40 +0530

    added missing commands in docker compose

--- Backend/utils/ffmpeg.queue.js
+++ Backend/utils/ffmpeg.queue.js
@@ -6,8 +6,8 @@ configDotenv();
 const isProduction = process.env.NODE_ENV === 'PRODUCTION' ;
 
 const connection = {
-  host : isProduction ? 'redis' : 'localhost' ,
-  port : 6379
+  host : isProduction ? process.env.REDIS_HOST : 'localhost' ,
+  port : process.env.REDIS_PORT
 }
 
 const ffmpegQueue = new Queue('video_process', {connection}) ;
